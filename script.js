8333bab60734d206d5210dcf1766c112c4e129d0
Yajhaira <yajhi2002@gmail.com>
2022-09-08 21:23:10 -0500

    Añade cambios

---
+++ script.js
@@ -0,0 +1,26 @@
+const date_picker_element = document.querySelector('.date-picker');
+const selected_date_element = document.querySelector('.date-picker .selected-date');
+const dates_element = document.querySelector('.date-picker .dates');
+
+//EVENTS LISTENERS
+date_picker_element.addEventListener('click', toggleDatePicker);
+
+//FUNCTIONS
+function toggleDatePicker (e) {
+    console.log(e.path);
+
+    if(!checkEventPathForClass(e.path, 'dates')) {
+        dates_element.classList.toggle('active');
+    }
+}
+
+//HELPER FUNCTIONS
+function checkEventPathForClass (path, selector) {
+    for (let i = 0; i < path.length; i++) {
+        if(path[i].classList && path[i].classList.contains(selector)) {
+            return true;
+        }
+    }
+
+    return false;
+}
